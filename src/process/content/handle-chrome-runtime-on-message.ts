601378e1f3cd3947f9b4c40967844e7fa03674a8
hotsuma kawashima <hkawashimajob1992@gmail.com>
2022-06-05 23:03:12 +0900

    contentでclickのイベントを取得できないことの修正

--- src/process/content/handle-chrome-runtime-on-message.ts
+++ src/process/content/handle-chrome-runtime-on-message.ts
@@ -2,9 +2,11 @@ import { call, take, put } from 'redux-saga/effects'
 import { Action } from '~/action'
 import * as fromChromeRuntimeOnMessageChannelProcess from '~/process/channel/chrome-runtime-on-message'
 import * as fromChromeTabsOnUpdatedProcessAction from '~/action/process/channel/chrome-tabs-on-updated'
+import * as fromChromeActionOnClickedChannelProcessAction from '~/action/process/channel/chrome-action-on-clicked'
 
 export const actions = [
   fromChromeTabsOnUpdatedProcessAction.TAB_STATUS_LOADING,
+  fromChromeActionOnClickedChannelProcessAction.ON_CLICK_EXTENTION,
 ]
 
 export function* watchHandleChromeRuntimeOnMessage(
